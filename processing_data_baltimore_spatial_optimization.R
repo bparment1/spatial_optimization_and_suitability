b37e28185e7404d21337ae95696715594d659cff
Benoit Parmentier <bparmentier@sshgw02.research.sesync.org>
2018-11-03 14:30:00 -0400

    matching dataset in resolution and projection system with gdal

--- processing_data_baltimore_spatial_optimization.R
+++ processing_data_baltimore_spatial_optimization.R
@@ -178,7 +178,7 @@ plot(r_lc_crop)
 plot(reg_sf_lc$geometry,add=T)
 
 lc_crop_baltimore_filename <- paste0("r_lc_crop_",out_suffix,file_format)
-writeRaster(r_lc_crop,filename = file.path(out_dir,dem_lc_baltimore_filename))
+writeRaster(r_lc_crop,filename = file.path(out_dir,lc_crop_baltimore_filename))
 
 #########################
 ### PART 3: reproject raster to Maryland State plane #######
@@ -193,7 +193,6 @@ dataType_table <- generate_raster_dataType_table()
 #r_lc_md <- projectRaster(r_lc_crop,r_dem_md)
 
 #### Use gdal:reproject dem first 
-
 
 data_type_str <- dataType(r_dem)
 NAvalue(r_dem)
@@ -242,15 +241,105 @@ CRS_reg <- crs_reg
 
 #gdalwarp -t_srs '+proj=utm +zone=11 +datum=WGS84' -overwrite raw_spot.tif utm11.tif
 
+#gdalsrsinfo -o wkt other.tif > target.wkt
+cmd_str <- paste0("gdalsrsinfo ",
+                  "-o wkt ", 
+                  dem_md_baltimore,
+                  " > ", "target.wkt")
+out <- system(cmd_str)
+res_val <- res(r_dem_md)
+ncol(r_dem_md)
+ts_val <- rev(dim(r_dem_md)[1:2])
+extent_val <- extent(r_dem_md)
+#> extent(r_dem_md)
+#class       : Extent 
+#xmin        : 408279.5 
+#xmax        : 452098.3 
+#ymin        : 168605.6 
+#ymax        : 201700 
+
+" -te <x_min> <y_min> <x_max> <y_max>",
+extent_gdal <- extent_val[c(1,3,2,4)]
+
+#-tr xres yres:
+#  set output file resolution (in target georeferenced units)
+#-tap:
+#  (GDAL >= 1.8.0) (target aligned pixels) align the coordinates of the extent of the output file to the values of the -tr, 
+#such that the aligned extent includes the minimum extent.
+#-ts width height:
+#  set output file size in pixels and lines. 
+#If width or height is set to 0, the other dimension will be guessed from the computed resolution. Note that -ts cannot be used with -tr
+
+## this does not work to match
 cmd_str = paste0("gdalwarp",
                  " -ot ",output_type,
                  " -srcnodata ",NA_flag_val_str,
-                 " -t_srs ", paste0("'",CRS_reg,"'"),
+                 " -t_srs ", "target.wkt",
+                 " -te ", paste0(extent_gdal,collapse=" "),
+                 " -tr ", paste0(res_val,collapse = " "),
+                 " -tap ",
+                 #" -ts ", paste0(ts_val,collapse = " "),
                  " -dstnodata ",NA_flag_val_str,
                  " -overwrite",
                  " ",src_dataset, 
                  " ",dst_dataset)             
 
+cmd_str = paste0("gdalwarp",
+                 " -ot ",output_type,
+                 " -srcnodata ",NA_flag_val_str,
+                 " -t_srs ", "target.wkt",
+                 " -te ", paste0(extent_gdal,collapse=" "),
+                 #" -tr ", paste0(res_val,collapse = " "),
+                 #" -tap ",
+                 " -ts ", paste0(ts_val,collapse = " "),
+                 " -dstnodata ",NA_flag_val_str,
+                 " -overwrite",
+                 " ",src_dataset, 
+                 " ",dst_dataset)      
+
+#$ gdalwarp -te <x_min> <y_min> <x_max> <y_max> input.bil clipped_output.bil
+
 system(cmd_str)
+
+r_lc_md <- raster(dst_dataset)
+extent(r_lc_md)
+extent(r_dem_md)
+res(r_lc_md)
+res(r_dem_md)
+dim(r_dem_md)
+dim(r_lc_md)
+#https://gis.stackexchange.com/questions/239013/reprojecting-a-raster-to-match-another-raster-using-gdal
+
+http://www.gdal.org/gdalwarp.html says
+
+-t_srs srs_def: target spatial reference set. The coordinate systems that can be passed are anything supported by the OGRSpatialReference.SetFromUserInput() call, which includes EPSG PCS and GCSes (i.e. EPSG:4296), PROJ.4 declarations (as above), or the name of a .prj file containing well known text.
+
+http://www.gdal.org/gdalsrsinfo.html says it
+
+Lists info about a given SRS in number of formats (WKT, PROJ.4, etc.)
+
+So you can write the SRS of your other raster to a file via
+
+gdalsrsinfo -o wkt other.tif > target.wkt
+and read it for gdalwarp via
+
+http://erouault.blogspot.com/2015/10/gdal-and-ogr-utilities-as-library.html
+
+http://erouault.blogspot.com/2015/10/gdal-and-ogr-utilities-as-library.html
+
+from osgeo import gdal
+
+ds = gdal.Open('input.tif')
+ds = gdal.Translate('output.tif', ds, projWin = [-75.3, 5.5, -73.5, 3.7])
+ds = None
+
+ds = gdal.Warp('', input_ds, format = 'VRT')
+
+
+gdalwarp -t_srs target.wkt source.tif output.tif
+You can combine this with a scripting language of your
+
+gdalsrsinfo -o wkt other.tif > target.wkt
+
 
 ################################### End of script #########################################
